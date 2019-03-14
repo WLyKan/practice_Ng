8e7a7069cd080bc59395385a6f6f0e6c331978b1
king.wang <king.wang@fansunion.com>
2019-03-14 20:00:34 +0800

    feat: add http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core'; // ng模块装饰器
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms'; // 引入ngModel
 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomerDashboardModule } from './customer-dashboard/customer-dashboard.module';
@@ -10,23 +12,29 @@ import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessageComponent } from './message/message.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { InMemoryDataService } from './in-memory-data.service';
 
 @NgModule({
-  declarations: [ // 声明组件
-    AppComponent,
-    HeroesComponent,
-    HeroDetailComponent,
-    MessageComponent,
-    DashboardComponent
-  ],
-  imports: [ // 引入全局模块
-    BrowserModule,
-    FormsModule,
-    HttpClientModule,
-    AppRoutingModule,
-    CustomerDashboardModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [ // 声明组件
+        AppComponent,
+        HeroesComponent,
+        HeroDetailComponent,
+        MessageComponent,
+        DashboardComponent,
+    ],
+    imports: [ // 引入全局模块
+        BrowserModule,
+        FormsModule,
+        HttpClientModule,
+        AppRoutingModule,
+        CustomerDashboardModule,
+        HttpClientInMemoryWebApiModule.forRoot(
+            InMemoryDataService, { dataEncapsulation: false }
+        ),
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
+// import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 // 这是一个典型的“服务中的服务”场景,
 // MessageService 注入到了 HeroService 中，而 HeroService 又被注入到了 HeroesComponent 中
@@ -13,15 +15,47 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
-    constructor(private messageService: MessageService) { }
+    private heroesUrl = 'api/heroes';
 
-    getHeroes(): Observable<Hero[]> {
-        this.messageService.add('HeroService: fetched heroes');
-        return of(HEROES); // 返回一个 Observable<Hero[]>，它会发出单个值，这个值就是这些模拟英雄的数组
+    constructor(
+        private http: HttpClient,
+        private messageService: MessageService
+    ) { }
+
+    // Log a HeroService message with the MessageService
+    private log(message: string) {
+        this.messageService.add(`HeroService: ${message}`);
     }
 
+    getHeroes(): Observable<Hero[]> {
+        // this.messageService.add('HeroService: fetched heroes');
+        // return of(HEROES); // 返回一个 Observable<Hero[]>，它会发出单个值，这个值就是这些模拟英雄的数组
+        return this.http.get<Hero[]>(this.heroesUrl)
+            .pipe(
+                tap(_ => this.log('fetched heroes')),
+                catchError(this.handleError<Hero[]>('getHeroes', []))
+            );
+    }
+
+    /** GET hero by id. Will 404 if id not found */
     getHero(id: number): Observable<Hero> {
-        this.messageService.add(`HeroService: fetched hero id=${id}`);
-        return of(HEROES.find(hero => hero.id === id));
+        // this.messageService.add(`HeroService: fetched hero id=${id}`);
+        // return of(HEROES.find(hero => hero.id === id));
+        const url = `${this.heroesUrl}/${id}`;
+        return this.http.get<Hero>(url).pipe(
+            tap(_ => this.log(`fetched hero id=${id}`)),
+            catchError(this.handleError<Hero>(`getHero id=${id}`))
+        );
+    }
+
+    private handleError<T>(operation = 'operation', result?: T) {
+        return (error: any): Observable<T> => {
+            // TODO: send the error to remote logging infrastructure
+            console.log(error);
+
+            this.log(`${operation} failed: ${error.message}`);
+
+            return of(result as T);
+        };
     }
 }
